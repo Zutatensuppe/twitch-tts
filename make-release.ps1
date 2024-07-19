034bd4961898a4e42e79febf1c5d83cf2ff8e65c
Zutatensuppe <oo.para@gmail.com>
2024-07-19 19:02:49 +0200

    adjustments for release creation

--- make-release.ps1
+++ make-release.ps1
@@ -2,7 +2,9 @@ rm -r -fo build
 rm -r -fo dist
 poetry run pyinstaller -F run.py
 cp config_example.jsonc dist/config.jsonc
-mv dist/run.exe dist/tts.exe
 cd dist
+mv run.exe tts.exe
 7z a -tzip ../build/$(poetry version -s).zip *
-gh release create $(poetry version -s) -t $(poetry version -s) -F build/$(poetry version -s).zip
+cd ..
+gh release create $(poetry version -s) -t $(poetry version -s) --generate-notes
+gh release upload $(poetry version -s) build/$(poetry version -s).zip

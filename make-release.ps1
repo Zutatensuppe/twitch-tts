ffb3633ead260d9b4e05e1ae58b01254c464b736
Zutatensuppe <oo.para@gmail.com>
2024-07-19 19:07:44 +0200

    improve make-release script

--- make-release.ps1
+++ make-release.ps1
@@ -1,10 +1,12 @@
+# this is a very simple powershell script to create a release
 rm -r -fo build
 rm -r -fo dist
 poetry run pyinstaller -F run.py
 cp config_example.jsonc dist/config.jsonc
 cd dist
 mv run.exe tts.exe
-7z a -tzip ../build/$(poetry version -s).zip *
+$version = (poetry version -s)
+7z a -tzip ../build/twitch-tts-$version.zip *
 cd ..
-gh release create $(poetry version -s) -t $(poetry version -s) --generate-notes
-gh release upload $(poetry version -s) build/$(poetry version -s).zip
+gh release create $version -t "twitch-tts-$version" --generate-notes
+gh release upload $version build/twitch-tts-$version.zip

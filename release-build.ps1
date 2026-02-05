3f33010fefd8211836ce6fffc6874435c92b8081
Zutatensuppe <oo.para@gmail.com>
2026-02-05 18:54:43 +0100

    fix version detection with uv

--- release-build.ps1
+++ release-build.ps1
@@ -5,6 +5,6 @@ uv run pyinstaller -F run.py
 cp config_example.jsonc dist/config.jsonc
 cd dist
 mv run.exe tts.exe
-$version = (uv run poetry version -s)
+$version = (uv version --short)
 7z a -tzip ../build/twitch-tts-$version.zip *
 cd ..
